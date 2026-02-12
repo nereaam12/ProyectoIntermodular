714dd39773ccace5b6262b4ad98716b4523ac28d
marope01 <marope01@alumnatflorida.es>
2026-02-12 13:43:59 +0100

    register conectado a una api

---
+++ CasaLuxe/src/Controller/RegistrationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/users', name: 'api_users_')]
+class RegistrationController extends AbstractController
+{
+    #[Route('', methods: ['POST'], name: 'create')]
+    public function create(
+        Request                     $request,
+        EntityManagerInterface      $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse
+    {
+
+        $data = json_decode($request->getContent(), true);
+
+        try {
+            $user = new User();
+            $user->setEmail($data['email']);
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $data['password']
+            );
+
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+
+            return new JsonResponse([
+                'message' => 'User created successfully'
+            ], 201);
+
+        } catch (UniqueConstraintViolationException $e) {
+
+            return new JsonResponse([
+                'error' => 'This email is already registered'
+            ], 409); // 409 = Conflict
+        }
+    }
+}
+

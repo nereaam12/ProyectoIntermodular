2d199ff4fef9204880a01850683264faaaa286cb
marope01 <marope01@alumnatflorida.es>
2026-02-16 08:44:10 +0100

    usuarios y creando clienetes

--- CasaLuxe/src/Entity/User.php
+++ CasaLuxe/src/Entity/User.php
@@ -21,6 +21,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180)]
     private ?string $email = null;
 
+
+
     /**
      * @var list<string> The user roles
      */

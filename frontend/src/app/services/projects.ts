803782360fccc48f2ded644b23968201e533133a
Marcos <marcosrodriguez@gmail.com>
2026-02-22 23:13:19 +0100

    falta despliegue

---
+++ frontend/src/app/services/projects.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Response } from '../interfaces/response.interface';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Projects {
+  private http = inject(HttpClient);
+  private auth = inject(AuthService);
+  private apiUrl = 'http://localhost:8000/api';
+  public getData(): Observable<Response[]> {
+    return this.http.get<Response[]>(`${this.apiUrl}/projects`, {
+      headers: this.auth.getAuthHeaders(), // ✅ enviamos token
+    });
+  }
+}
